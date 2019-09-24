9f50e05a553677f5fcd0698d394a8e4d75dfb15d
saiabhilash18 <55742034+saiabhilash18@users.noreply.github.com>
2019-09-24 17:52:08 +0530

    i did some changes

---
+++ Summer/src/summer/org/Sample.java
@@ -0,0 +1,17 @@
+package summer.org;
+
+import java.util.Scanner;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		String str="Hi";
+		Scanner ab= new Scanner(System.in);
+		System.out.println("enter your name");
+		String dd=ab.nextLine();
+	System.out.println(str+" "+dd+ " "+" how are you" );
+	//this is simple program
+
+	}
+
+}
